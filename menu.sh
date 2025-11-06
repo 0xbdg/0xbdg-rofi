171eef0a1957b06a1043465c66c08793ee84cf8f
0xbdg <benjamindguntara@protonmail.com>
2025-11-06 22:21:20 +0800

    diasjdoiasj

--- menu.sh
+++ menu.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-MAIN_MENU=" Applications\n󰖩 WiFi Settings\n Bluetooth Setting\n System"
+MAIN_MENU=" Applications\n󱚾 WiFi Settings\n Bluetooth Setting\n System"
 WIFI_MENU="󱛁 WiFi Scan\n󱚼 Disable WiFi\n󰈉 Connect to Hidden Network\n󰉉 Show saved WiFi\n Back"
-BLUETOOTH_MENU="Power:\nScan\nPairable\nDiscoverable\nBack"
 SYSTEM_MENU=" Shutdown\n Reboot\n󰒲 Sleep\n Back"
 
 wifi_menu(){
     WIFI=$(echo -e "$WIFI_MENU" | rofi -dmenu)
     case $WIFI in  
-            "󱛁 WiFi Scan") rofi -drun ;;
+            "󱛁 WiFi Scan")
+                notify-send "Scanning WiFi..."
+                WIFI_LIST=" Back\n󱛄 Rescan\n$(printf '#%.0s' {1..100})\n"
+                WIFI_LIST+=$(nmcli -t -f ssid d w l | grep -v '^$')
+
+                SELECTED_MENU=$(printf "%b" "$WIFI_LIST" | rofi -dmenu -p "Select SSID")
+
+                echo $SELECTED_MENU
+ 
+                case $SELECTED_MENU in
+                    " Back") wifi_menu ;;
+                    "󱛄 Rescan") 
+                        notify-send "Rescan WiFi..."
+                        nmcli device wifi rescan;;
+                    "$(printf '#%.0s' {1..100})") exit;;
+                    *) ;;
+                esac
+
+            ;;
             " Back") main_menu ;;
             *) exit ;;
     esac
@@ -31,7 +48,7 @@ main_menu(){
     MAIN=$(echo -e "$MAIN_MENU" | rofi -dmenu -p "0xbdg")
     case $MAIN in
         " Applications") rofi -show drun -show-icons ;;
-        "󰖩 WiFi Settings")
+        "󱚾 WiFi Settings")
             wifi_menu
         ;;
         " Bluetooth Setting") rofi -drun ;;
