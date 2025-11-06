894c70f9e202fdde9bb16387de6f0bafb913c13f
0xbdg <benjamindguntara@protonmail.com>
2025-11-06 12:27:37 +0800

    pasdpasij

--- menu.sh
+++ menu.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-MENU="Applications\nNetwork setting\nBluetooth setting"
-CHOICES=$(echo -e "$MENU" | rofi -dmenu -p "0xbdg")
+MENU=" Applications\n󰖩 WiFi Settings\n Bluetooth Setting\n System"
+WIFI_MENU="󱛁 WiFi Scan\n󱚼 Disable WiFi\n󰈉 Connect to Hidden Network\n󰉉 Show saved WiFi"
+BLUETOOTH_MENU=""
+SYSTEM_MENU=" Shutdown\n Reboot\n󰒲 Sleep"
 
-case $CHOICES in
-    Applications) rofi -show drun -show-icons ;;
-    *) exit ;;
+MAIN_MENU=$(echo -e "$MENU" | rofi -dmenu -p "0xbdg")
+WIFI=$(echo -e "$WIFI_MENU" | rofi -dmenu)
+BLUETOOTH=$()
 
-esac
+function main()
+    case $MAIN_MENU in
+        " Applications") rofi -show drun -show-icons ;;
+        "󰖩 WiFi Settings")
+            case $WIFI in 
+                "󱛁 WiFi Scan") rofi -drun ;;
+                *) exit ;;
+            esac
+        ;;
+        
+        *) exit ;;
+
+    esac
+main
