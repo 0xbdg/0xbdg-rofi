7b8a4e8655f371687f668f91a60e020dacb0a062
0xbdg <benjamindguntara@protonmail.com>
2025-11-06 15:19:13 +0800

    pdpaskdapok

--- menu.sh
+++ menu.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
-MENU=" Applications\n󰖩 WiFi Settings\n Bluetooth Setting\n System"
-WIFI_MENU="󱛁 WiFi Scan\n󱚼 Disable WiFi\n󰈉 Connect to Hidden Network\n󰉉 Show saved WiFi"
-BLUETOOTH_MENU=""
-SYSTEM_MENU=" Shutdown\n Reboot\n󰒲 Sleep"
+MAIN_MENU=" Applications\n󰖩 WiFi Settings\n Bluetooth Setting\n System"
+WIFI_MENU="󱛁 WiFi Scan\n󱚼 Disable WiFi\n󰈉 Connect to Hidden Network\n󰉉 Show saved WiFi\n Back"
+BLUETOOTH_MENU="Power:\nScan\nPairable\nDiscoverable\nBack"
+SYSTEM_MENU=" Shutdown\n Reboot\n󰒲 Sleep\n Back"
 
-MAIN_MENU=$(echo -e "$MENU" | rofi -dmenu -p "0xbdg")
-WIFI=$(echo -e "$WIFI_MENU" | rofi -dmenu)
-BLUETOOTH=$()
+wifi_menu(){
+    WIFI=$(echo -e "$WIFI_MENU" | rofi -dmenu)
+    case $WIFI in  
+            "󱛁 WiFi Scan") rofi -drun ;;
+            " Back") main_menu ;;
+            *) exit ;;
+    esac
+}
 
-function main()
-    case $MAIN_MENU in
+bluetooth_menu(){
+    BLUETOOTH=""
+}
+
+system_menu(){
+    SYSTEM=$(echo -e "$SYSTEM_MENU" | rofi -dmenu)
+    case $SYSTEM in 
+        " Shutdown") ;;
+        " Back") main_menu ;;
+        *) exit ;;
+    esac
+}
+
+main_menu(){
+    MAIN=$(echo -e "$MAIN_MENU" | rofi -dmenu -p "0xbdg")
+    case $MAIN in
         " Applications") rofi -show drun -show-icons ;;
         "󰖩 WiFi Settings")
-            case $WIFI in 
-                "󱛁 WiFi Scan") rofi -drun ;;
-                *) exit ;;
-            esac
+            wifi_menu
+        ;;
+        " Bluetooth Setting") rofi -drun ;;
+        " System")
+            system_menu 
         ;;
         
         *) exit ;;
 
     esac
-main
+}
+
+main_menu
